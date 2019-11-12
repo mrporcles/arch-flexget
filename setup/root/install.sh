b4c52bb899c1a5e7bb9420e86cec5c2b2dc2d985
mrporcles <mrporcles@gmail.com>
2019-11-12 14:43:26 +0000

    Update install.sh
    Changed to work with flexget version 3. Python v2 no longer required.

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,13 +4,13 @@
 set -e
 
 # define pacman packages
-pacman_packages="python2 python2-pip"
+pacman_packages="python python-setuptools python-pip supervisor"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
-pip2 install --upgrade setuptools
-pip2 install flexget
-pip2 install transmissionrpc
+#pip2 install --upgrade setuptools
+pip install flexget
+#pip2 install transmissionrpc
 
 # call aor script (arch official repo)
 # source /root/aur.sh
